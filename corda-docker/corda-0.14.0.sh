75f69004537613c00d04ca4d7cc52f122823420d
Markus Klems <markus@Markuss-MacBook-Pro-4.local>
2017-08-29 16:11:44 +0200

    Replaced openjdk with oracle-java8 in an attempt to get the multi-node cordapp setup running (still does not work with more than 2 nodes).

--- corda-docker/corda-0.14.0.sh
+++ corda-docker/corda-0.14.0.sh
@@ -6,4 +6,4 @@ export JAVA_OPTIONS="${JAVA_OPTIONS--Xmx512m}"
 export JAVA_DCAPSULE="${JAVA_DCAPSULE--agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$DEBUG_PORT}"
 export JAVA_DNAME="${JAVA_DNAME-$NODE_NAME-corda.jar}"
 
-exec java $JAVA_OPTIONS -Dname="$JAVA_DNAME" -Dcapsule.jvm.args="$JAVA_DCAPSULE" -jar /opt/corda/corda.jar --log-to-console --logging-level=DEBUG
+exec java $JAVA_OPTIONS -Dname="$JAVA_DNAME" -Dcapsule.jvm.args="$JAVA_DCAPSULE" -jar /opt/corda/corda.jar --log-to-console --logging-level=INFO
